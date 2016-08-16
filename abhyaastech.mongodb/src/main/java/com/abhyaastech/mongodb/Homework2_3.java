7b1492cbf023904649e1ac7b58e4153d966bf0e1
ngvinay <vinay.ng.mys@gmail.com>
2016-08-15 21:31:54 -0400

    final changes for 2.3

--- abhyaastech.mongodb/src/main/java/com/abhyaastech/mongodb/Homework2_3.java
+++ abhyaastech.mongodb/src/main/java/com/abhyaastech/mongodb/Homework2_3.java
@@ -3,10 +3,12 @@ import com.mongodb.MongoClient;
 //import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
+
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Projections;
 import com.mongodb.client.model.Sorts;
+import com.mongodb.client.result.DeleteResult;
 import com.mongodb.util.JSON;
 
 import freemarker.template.Configuration;
@@ -33,23 +35,37 @@ public class Homework2_3 {
 	public static void main(String[] args) {
 		
 		MongoClient client = new MongoClient();
-		Object prev_id="";
+		Integer prev_id=-1;
+		int counter=0;
+		int deletecount=0;
         MongoDatabase database = client.getDatabase("students");
         final MongoCollection<Document> collection = database.getCollection("grades");
         Bson sort = Sorts.orderBy(Sorts.ascending("student_id"),Sorts.ascending("score"));
         //Bson projection = Projections.fields(include("student_id"));
         List<Document> cur = collection.find(new Document("type","homework")).
         		sort(sort)
-        		.projection(new Document("student_id",1).append("_id",1))
+        		//.projection(new Document("student_id",1).append("_id",1))
         		.into(new ArrayList<Document>());
         for(Document all:cur){
-        	if(all.get("student_id") != prev_id){
-        		System.out.println(JSON.serialize(all.get("_id")));
-        	}
-        	    	
+        	//if(((Integer)all.get("student_id")) != prev_id){
+        	//	System.out.println(((Integer)all.get("student_id")));
+        	//	System.out.println(prev_id);
+        		if (counter == 0){
+        		System.out.println(JSON.serialize(all));
+        		Object id = all.get("_id");
+        		//DeleteResult d=
+        		collection.deleteOne(Filters.eq("_id", id));
+        		deletecount=deletecount+1;
+        		}
+        		counter=counter+1;
+        		if(counter==2){
+        			counter=0;
+        		}
+        	
+        	//prev_id=(Integer)all.get("student_id");
         }
             
-        
+        System.out.println(deletecount);
         //collection.deleteMany(eq("type","homework"));      
 			}
 
